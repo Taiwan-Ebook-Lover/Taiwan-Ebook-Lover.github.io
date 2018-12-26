668ec4bb2eca7c6c4d74e35b09e37ebdea98ac4d
Jerry Wang <focaaby@gmail.com>
2018-12-27 02:29:09 +0800

    Add landing page

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,20 @@
   <v-app>
     <v-toolbar app color="#0eb29a">
       <v-toolbar-title class="white--text">
-        <v-avatar tile="true" class="mx-2">
-          <img src="./assets/ebook-logo.png" alt="ebook-logo">
+        <v-avatar tile="true" class="ma-2">
+          <img src="./assets/ebook-logo.svg" alt="ebook-logo">
         </v-avatar>
         <span>台灣電子書搜尋</span>
       </v-toolbar-title>
       <v-spacer/>
 
-      <v-toolbar-items class="hidden-sm-and-down mr-2">
+      <v-toolbar-items :class="iconShow">
         <a class="img-flex" href="https://play.google.com/store/apps/details?id=liou.rayyuan.ebooksearchtaiwan">
           <img src="./assets/play-store-badge.svg" alt="play-store">
         </a>
       </v-toolbar-items>
 
-      <v-toolbar-items class="hidden-sm-and-down">
+      <v-toolbar-items :class="iconShow">
         <a class="img-flex" href="https://itunes.apple.com/tw/app/%E5%8F%B0%E7%81%A3%E9%9B%BB%E5%AD%90%E6%9B%B8%E6%90%9C%E5%B0%8B/id1279690327">
           <img src="./assets/app-store-badge.svg" alt="app-store">
         </a>
@@ -26,12 +26,27 @@
 
 </template>
 
+<script>
+export default {
+  name: 'ebook-search',
+  computed: {
+    iconShow() {
+      return this.$route.path === '/' ? 'disable-all' : 'hidden-sm-and-down mr-2';
+    },
+  },
+};
+</script>
+
 <style lang="scss" scoped>
 .view-top {
   margin-top: 58px;
 }
 .img-flex {
   display: flex;
+}
+
+.disable-all {
+  display: none;
 }
 </style>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,63 @@
 <template>
-  <v-container grid-list-md>
-    <div>Waiting for ui</div>
+  <v-container text-md-center text-sm-center text-xs-center>
+    <v-layout wrap justify-center my-4>
+      <v-avatar tile="true" size="256" class="mx-3 my-4">
+        <img src="../assets/ebook-logo.svg" alt="ebook-logo">
+      </v-avatar>
+      <v-flex shrink my-auto>
+        <div class="display-3 mb-4">台灣電子書搜尋</div>
+        <v-flex>
+          <v-text-field
+            v-model="searchword"
+            label="Solo"
+            placeholder="搜尋您想比價的電子書名關鍵字或 ISBN"
+            append-icon="search"
+            solo
+            clearable
+            @keyup.enter.native='redirectToSearch'
+          ></v-text-field>
+        </v-flex>
+        <v-flex>
+          <v-btn color="#0eb29a" class="white--text" large @click="redirectToSearch">開始搜尋</v-btn>
+
+          <div class="img-store">
+            <a class="img-flex" href="https://play.google.com/store/apps/details?id=liou.rayyuan.ebooksearchtaiwan">
+              <img class="mt-1" src="../assets/play-store-badge.svg" alt="play-store">
+            </a>
+          </div>
+          <div class="img-store">
+             <a class="img-flex" href="https://itunes.apple.com/tw/app/%E5%8F%B0%E7%81%A3%E9%9B%BB%E5%AD%90%E6%9B%B8%E6%90%9C%E5%B0%8B/id1279690327">
+              <img class="mt-1" src="../assets/app-store-badge.svg" alt="app-store">
+            </a>
+          </div>
+        </v-flex>
+
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
   name: 'home',
-  components: {
-    // HelloWorld,
+  data: () => ({
+    searchword: '',
+  }),
+  methods: {
+    redirectToSearch() {
+      if (this.searchword === '') return;
+      this.$router.push({ path: 'search', query: { q: this.searchword } });
+    },
   },
 };
 </script>
+
+<style scoped>
+.img-store {
+  display: inline-flex;
+  vertical-align: middle;
+  align-items: center;
+  justify-content: center;
+  margin: 8px 12px;
+}
+</style>
