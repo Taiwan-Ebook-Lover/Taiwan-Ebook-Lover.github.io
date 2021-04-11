3e31c009228a30900821bdb4e5be4dadbb0f7d58
SafeFly <ast850328@gmail.com>
2021-04-11 13:47:36 +0800

    feat: import mdi font

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import vuetify from './plugins/vuetify';
 import App from './App.vue';
 import router from './router';
+import '@mdi/font/css/materialdesignicons.css';
 
 Vue.config.productionTip = false;
 
