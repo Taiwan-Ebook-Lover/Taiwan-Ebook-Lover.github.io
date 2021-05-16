049e370121bf4445e55b537fa97e17de76c36ba7
Yuer Lee <passerbyid@gmail.com>
2021-05-16 16:54:11 +0800

    feat: adjust mdi-icon structure

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue';
 import vuetify from './plugins/vuetify';
 import App from './App.vue';
 import router from './router';
-import '@mdi/font/css/materialdesignicons.css';
 
 Vue.config.productionTip = false;
 
