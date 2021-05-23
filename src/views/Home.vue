020af1e5c87dce14a1605bd0a0035f062c184c20
Yuer Lee <passerbyid@gmail.com>
2021-05-24 05:41:51 +0800

    Add prettier support (#28) (#31)
    * Chore: Add prettier with husky & lint-staged (#28)
    * Style: Format all files by prettier
    * CI: Add lint workflow

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,9 @@
 <template>
   <v-container text-md-center text-sm-center text-xs-center fluid>
     <v-layout row wrap mt-5>
-
       <v-flex lg2 offset-lg5 sm4 offset-sm4 xs6 offset-xs3>
-          <v-img
-            :src="require('@/assets/ebook-logo-bg.svg')"
-            alt="ebook-logo"
-            aspect-ratio="1"
-          >
-          </v-img>
+        <v-img :src="require('@/assets/ebook-logo-bg.svg')" alt="ebook-logo" aspect-ratio="1">
+        </v-img>
       </v-flex>
 
       <v-flex my-2 xs12>
@@ -16,16 +11,16 @@
       </v-flex>
 
       <v-flex my-3 lg6 offset-lg3 sm8 offset-sm2>
-        <v-form ref="form" @submit.prevent="redirectToSearch" >
+        <v-form ref="form" @submit.prevent="redirectToSearch">
           <v-text-field
-              v-model="searchWord"
-              placeholder="搜尋您想比價的電子書名關鍵字或 ISBN"
-              append-icon="search"
-              @click:append="redirectToSearch"
-              solo
-              clearable
-              hide-details
-            ></v-text-field>
+            v-model="searchWord"
+            placeholder="搜尋您想比價的電子書名關鍵字或 ISBN"
+            append-icon="search"
+            @click:append="redirectToSearch"
+            solo
+            clearable
+            hide-details
+          ></v-text-field>
         </v-form>
       </v-flex>
 
@@ -41,14 +36,8 @@
           multiple
           clearable
         >
-          <template
-            slot="selection"
-            slot-scope="{ item, index }"
-          >
-            <v-chip
-              close
-              @click:close="remove(index)"
-            >
+          <template slot="selection" slot-scope="{ item, index }">
+            <v-chip close @click:close="remove(index)">
               <span>{{ item.displayName }}</span>
             </v-chip>
           </template>
@@ -57,21 +46,16 @@
 
       <v-flex xs12>
         <div class="headline grey--text">
-
           <span class="hidden-xs-only">
-            <i class="fab fa-twitter"></i> 在<a class="theme-color" href="https://twitter.com/TaiwanEBook">推特</a>上追蹤我們
+            <i class="fab fa-twitter"></i>
+            在<a class="theme-color" href="https://twitter.com/TaiwanEBook">推特</a>上追蹤我們
           </span>
 
           <span class="hidden-xs-only mx-4 vertical-divider"></span>
 
           <span>
             看看支援哪
-            <v-tooltip
-              bottom
-              color="white"
-              max-width="420"
-              v-model="isTooltipShow"
-            >
+            <v-tooltip bottom color="white" max-width="420" v-model="isTooltipShow">
               <template v-slot:activator="{ on, attrs }">
                 <span
                   v-bind="attrs"
@@ -80,30 +64,27 @@
                   @mouseover="handleTooltip"
                   @mouseleave="handleTooltip"
                 >
-                    {{bookstores.length}} 間台灣線上電子書店
+                  {{ bookstores.length }} 間台灣線上電子書店
                 </span>
               </template>
 
               <v-layout row wrap class="activated-stores-tooltip">
-                <v-flex v-for="(bookstore, key) in bookstores" :key='key' xs6 my-2>
-                  <v-avatar
-                    size="32">
-                    <img :src="`img/${bookstore.id}.png`" :alt="bookstore.displayName">
+                <v-flex v-for="(bookstore, key) in bookstores" :key="key" xs6 my-2>
+                  <v-avatar size="32">
+                    <img :src="`img/${bookstore.id}.png`" :alt="bookstore.displayName" />
                   </v-avatar>
                   <span class="ma-2 grey--text text--darken-1">
                     {{ bookstore.displayName }}
                   </span>
-                  <span class='float-right mr-3'>
-                    {{bookstore.isOnline? '🟢' : '🔴'}}
+                  <span class="float-right mr-3">
+                    {{ bookstore.isOnline ? '🟢' : '🔴' }}
                   </span>
                 </v-flex>
               </v-layout>
             </v-tooltip>
           </span>
-
         </div>
       </v-flex>
-
     </v-layout>
   </v-container>
 </template>
