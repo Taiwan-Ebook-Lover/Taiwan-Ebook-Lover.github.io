514bfa4c85d9b2a05045be9d2a79ec14a45afa4f
lemon5920 <lemon5920@gmail.com>
2021-09-19 18:43:08 +0800

    feat: Add linter and pre-commit setting.

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'no-console': 'warn',
+    indent: ['warn', 2],
+    quotes: ['warn', 'single'],
+    semi: ['warn', 'always'],
+    'comma-dangle': ['warn', 'always-multiline'],
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-empty-interface': [
+      'error',
+      {
+        allowSingleExtends: true,
+      },
+    ],
+  },
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import { FunctionComponent } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App: FunctionComponent = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +21,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
