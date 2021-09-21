4ea11cd3d66591b5d21249a61492ef48760f7587
lemon5920 <lemon5920@gmail.com>
2021-09-21 18:33:01 +0800

    feat: Add common theme config file.

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,15 @@ const gulp = require('gulp');
 const cache = require('gulp-cached');
 const cleanCSS = require('gulp-clean-css');
 const debug = require('gulp-debug');
+const gulpif = require('gulp-if');
 const gulpless = require('gulp-less');
 const postcss = require('gulp-postcss');
 const NpmImportPlugin = require('less-plugin-npm-import');
 
-gulp.task('less-compile', (done) => {
-  gulp
+const compilePipe = (isCommonTheme) => {
+  return gulp
     .src('src/themes/*-theme.less')
-    .pipe(cache('less-to-css'))
+    .pipe(gulpif(!isCommonTheme, cache('less-to-css')))
     .pipe(debug({ title: 'Less files:' }))
     .pipe(
       gulpless({
@@ -21,12 +22,21 @@ gulp.task('less-compile', (done) => {
     .pipe(postcss([autoprefixer()]))
     .pipe(cleanCSS())
     .pipe(gulp.dest('./public'));
+};
 
+gulp.task('themes-compile', (done) => {
+  compilePipe(false);
   done();
 });
 
-gulp.task('watch-themes-change', function () {
-  gulp.watch('src/themes/*-theme.less', gulp.series('less-compile'));
+gulp.task('common-theme-compile', (done) => {
+  compilePipe(true);
+  done();
 });
 
-gulp.task('auto-update-theme', gulp.series('less-compile', 'watch-themes-change'));
+gulp.task('watch-themes-change', () => {
+  gulp.watch('src/themes/*-theme.less', gulp.series('themes-compile'));
+  gulp.watch('src/themes/common.less', gulp.series('common-theme-compile'));
+});
+
+gulp.task('auto-update-theme', gulp.series('themes-compile', 'watch-themes-change'));
