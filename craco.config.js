bc2e7995708336b0ef8970303acc489a4961f390
lemon5920 <lemon5920@gmail.com>
2021-09-21 22:23:44 +0800

    feat: Add new path alias.

--- craco.config.js
+++ craco.config.js
@@ -3,8 +3,9 @@ const path = require('path');
 module.exports = {
   webpack: {
     alias: {
-      '@assets': path.resolve(__dirname, 'src/assets'),
       '@': path.resolve(__dirname, 'src/'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@customTypes': path.resolve(__dirname, 'src/assets/types'),
     },
   },
 };
