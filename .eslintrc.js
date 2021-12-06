af9541b924bf465608d460e04a5bdb26f8a2d32f
lemon5920 <lemon5920@gmail.com>
2021-12-06 11:35:57 +0800

    fix: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,33 +7,21 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:@typescript-eslint/recommended',
     'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
+    ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react', '@typescript-eslint'],
   rules: {
     'no-console': 'warn',
     indent: ['warn', 2],
     quotes: ['warn', 'single'],
     semi: ['warn', 'always'],
     'comma-dangle': ['warn', 'always-multiline'],
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-uses-react': 'off',
-    'react/prop-types': 'off',
-    '@typescript-eslint/no-empty-interface': [
-      'error',
-      {
-        allowSingleExtends: true,
-      },
-    ],
   },
   settings: {
     react: {
