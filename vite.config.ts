cf27dd66280e8c21daaf3b51c594c1eea9065fdd
lemon5920 <lemon5920@gmail.com>
2022-01-04 23:09:20 +0800

    feat: Support EJS template in index.html.

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,56 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
 import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
 import checker from 'vite-plugin-checker';
 
-export default defineConfig({
-  define: {
-    __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
-  },
-  plugins: [
-    react(),
-    checker({
-      eslint: {
-        files: ['./src'],
-        extensions: ['.tsx', '.ts'],
+export default defineConfig(({ mode }) => {
+  // Ref: https://github.com/vitejs/vite/issues/3105#issuecomment-939703781
+  const env = loadEnv(mode, 'env');
+  const htmlPlugin = () => {
+    return {
+      name: 'html-transform',
+      transformIndexHtml(html: string) {
+        return html.replace(/<%=(.*?)%>/g, function (match, p1) {
+          return env[p1];
+        });
       },
-    }),
-  ],
-  build: {
-    outDir: 'build',
-  },
-  server: {
-    open: '/',
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src/'),
-      '@api': path.resolve(__dirname, 'src/api'),
-      '@assets': path.resolve(__dirname, 'src/assets'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@customTypes': path.resolve(__dirname, 'src/assets/types'),
-      '@hooks': path.resolve(__dirname, 'src/hooks'),
-      '@layouts': path.resolve(__dirname, 'src/layouts'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
-      '@recoil': path.resolve(__dirname, 'src/recoil'),
-      '@routes': path.resolve(__dirname, 'src/routes'),
-      '@utils': path.resolve(__dirname, 'src/utils'),
+    };
+  };
+
+  return {
+    define: {
+      __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
     },
-  },
+    plugins: [
+      react(),
+      checker({
+        eslint: {
+          files: ['./src'],
+          extensions: ['.tsx', '.ts'],
+        },
+      }),
+      htmlPlugin(),
+    ],
+    build: {
+      outDir: 'build',
+    },
+    server: {
+      open: '/',
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src/'),
+        '@api': path.resolve(__dirname, 'src/api'),
+        '@assets': path.resolve(__dirname, 'src/assets'),
+        '@components': path.resolve(__dirname, 'src/components'),
+        '@customTypes': path.resolve(__dirname, 'src/assets/types'),
+        '@hooks': path.resolve(__dirname, 'src/hooks'),
+        '@layouts': path.resolve(__dirname, 'src/layouts'),
+        '@pages': path.resolve(__dirname, 'src/pages'),
+        '@recoil': path.resolve(__dirname, 'src/recoil'),
+        '@routes': path.resolve(__dirname, 'src/routes'),
+        '@utils': path.resolve(__dirname, 'src/utils'),
+      },
+    },
+  };
 });
